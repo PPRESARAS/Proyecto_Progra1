269b763d11685fa99ebcb4988db8620e260e8c07
matias <mkruger@udesa.edu.ar>
2019-11-07 16:37:21 -0300

    S

---
+++ js/listado.js
@@ -0,0 +1,21 @@
+var api_key = "8eaabce657eccc6be932f97172c1a728"
+
+fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=8eaabce657eccc6be932f97172c1a728&language=en-US")
+  .then(function(response){
+    return response.json();
+  })
+
+  .then (function(myJson){
+    console.log(myJson);
+    for (var i = 0; i < myJson.genres.length; i++) {
+      myJson.genres[i]
+      console.log(  myJson.genres[i].name )
+
+      var elementoHTML = document.querySelector('.listadoDeSeries')
+      var insertar = '<li>'
+      insertar += '<a href="genero.html" id="generos">'+myJson.genres[i].name+'</a>' //quiero que con cada genero me tire una foto que lo represente
+      insertar += '</li>'
+
+      elementoHTML.innerHTML += insertar
+   }
+  })
