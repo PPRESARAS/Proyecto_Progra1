f64ace51127eaf97b980793c831cd2435a445336
Pedro <ppresaras@udesa.edu.ar>
2019-11-12 17:10:02 -0300

    hipervinculo al logo

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,11 +1,10 @@
 function validar(){
-   	//valido el nombre
    	if (document.formulario.generos.value.length==0){
       		alert("Tienes que elegir un genero!")
       		document.formulario.generos.focus()
       		return 0;
    	}
-   	//el formulario se envia
+   	//el formulario se envia//
    	alert("Muchas gracias por enviar el formulario");
    	document.formulario.submit();
 }
