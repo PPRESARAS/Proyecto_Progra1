7063b324321f77a33e029f01187c9ce590dc40fa
Marcos Estrada <mestrada@udesa.edu.ar>
2019-11-13 23:07:08 -0300

    Resultado buscador simple.

---
+++ js/searchbar.js
@@ -0,0 +1,25 @@
+var queryString = location.search;
+var searchParams = new URLSearchParams(queryString);
+var search = searchParams.get("search");
+
+var url = "https://api.themoviedb.org/3/search/tv?api_key=8eaabce657eccc6be932f97172c1a728&language=en-US&query="+ search +"&page=1";
+
+
+fetch(url)
+  .then(function(respuesta){
+    return respuesta.json();
+  })
+  .then(function(datos){
+    console.log(datos);
+    for(var i=0; i<datos.results.length; i++){
+    datos.results[i]
+    var destino = document.querySelector(".resultados");
+    //ar datosFinales = datos.data;
+    var li = ""
+    li += "<li>"
+    li += datos.results[i].name
+    li += "</li>"
+   }
+
+  destino.innerHTML = li
+})
