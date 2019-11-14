cf572e34fb400777b1ee13ebe9cf0c1b0d684999
Marcos Estrada <mestrada@udesa.edu.ar>
2019-11-14 15:26:20 -0300

    SearchBar toques finales. Falta la precarga, el nuevo bar y css

--- js/searchbar.js
+++ js/searchbar.js
@@ -5,20 +5,40 @@ var search = searchParams.get("search");
 var url = "https://api.themoviedb.org/3/search/tv?api_key=8eaabce657eccc6be932f97172c1a728&language=en-US&query="+ search +"&page=1";
 
 
+var destinoh1 = document.querySelector(".textobuscado");
+var texto = '<h1>'+"Resultados para: "+search +'</h1>'
+  destinoh1.innerHTML += texto
+
+var destinoprecarga = document.querySelector(".form-groupSearch");
+var precarga = '<input type="search" class="form-control" name="search" value='+search+'>'
+  destinoprecarga.innerHTML += precarga
+
+var barraborrar = document.querySelector(".form-groupSearch")
+var barraborrada = barraborrar.children;
+var borrada = barraborrada[2]
+barraborrar.removeChild(borrada)
+
+
 fetch(url)
   .then(function(respuesta){
     return respuesta.json();
   })
   .then(function(datos){
-    console.log(datos);
+    //console.log(datos);
+    var posterURL = 'https://image.tmdb.org/t/p/original'
     for(var i=0; i<datos.results.length; i++){
     datos.results[i]
+    //console.log(datos.results[i])
     var destino = document.querySelector(".resultados");
     var li = ""
-    li += "<li>"
-    li += datos.results[i].name
+    li += "<li class='unidad'>"
+    li += '<img class="poster" src="'+posterURL+ datos.results[i].poster_path+'" alt=""</a>'
+    li += '<a href="detalles.html?id='+datos.results[i].id+'" class="tituloserie">'+datos.results[i].name+'</a>'
     li += "</li>"
+
+      destino.innerHTML += li
+
    }
 
-  destino.innerHTML = li
+
 })
